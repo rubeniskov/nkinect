9e45bbe8cc0cf0b4c3ebba73ad533e48c9ec9312
Rubén López Gomez <me@rubeniskov.com>
2017-08-21 04:47:08 +0200

    update: examples wirh streams

--- examples/web-stream-viewer/server.js
+++ examples/web-stream-viewer/server.js
@@ -1,5 +1,6 @@
 var kinect = require('../../')();
 var jpegJs = require('jpeg-js')
+var through = require('through')
 var bodyParser = require('body-parser');
 var browserify = require('browserify-middleware');
 var http = require('http');
@@ -17,9 +18,76 @@ var width = 640
 var height = 480;
 var boundaryID = "BOUNDARY";
 
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+var reArrangeBuffer = (type) => {
+  return through(function(chunk, enc, done) {
+    var data = new Buffer(width * height * 4);
+
+    for (var i = 0; i < width * height; i++) {
+      if (type === 'depth') {
+        var depth = (chunk[2 * i] + chunk[2 * i + 1] * 255) / 5;
+        data[4 * i] = depth;
+        data[4 * i + 1] = depth;
+        data[4 * i + 2] = depth;
+      } else {
+        data[4 * i] = chunk[3 * i];
+        data[4 * i + 1] = chunk[3 * i + 1];
+        data[4 * i + 2] = chunk[3 * i + 2];
+      }
+      data[4 * i + 3] = 255;
+      // data[4*i]   = type === 'depth' ? (depth = (chunk[2*i]+chunk[2*i+1]*255)/5) : chunk[3*i];
+      // data[4*i+1] = type === 'depth' ? depth : chunk[3*i+1];
+      // data[4*i+2] = type === 'depth' ? depth : chunk[3*i+2];
+      // data[4*i+3] = type === 'depth' ? 255 : chunk[3*i+2];;
+    }
+
+    this.push(data);
+    done && done();
+  })
+}
+var encodeJPEG = () => {
+  return through(function(chunk, enc, done) {
+    this.push(Buffer(jpegJs.encode({
+      data: chunk,
+      width: width,
+      height: height
+    }, 80).data));
+    done && done();
+  });
+}
+
+var encodeMJPEG = () => {
+  return through(function(chunk, enc, done) {
+    this.push('--' + boundaryID + '\r\n')
+    this.push('Content-Type: image/jpeg\r\n');
+    this.push('Content-Length: ' + chunk.length + '\r\n');
+    this.push("\r\n");
+    this.push(chunk);
+    this.push("\r\n");
+    done && done();
+  });
+}
+
+var channelHandler = (type) => {
+  return function(req, res) {
+    res.writeHead(200, {
+      'Content-Type': 'multipart/x-mixed-replace;boundary="' + boundaryID + '"',
+      'Connection': 'keep-alive',
+      'Expires': 'Fri, 27 May 1977 00:00:00 GMT',
+      'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
+      'Pragma': 'no-cache'
+    });
+
+    var stream = type === 'depth' ? kinect.createDepthStream() : kinect.createVideoStream();
+
+    stream.pipe(reArrangeBuffer(type)).pipe(encodeJPEG()).pipe(encodeMJPEG()).pipe(res);
+
+    req.on('close', function() {
+      stream.unpipe();
+    });
+  }
+}
+
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(bodyParser.json());
 
@@ -29,99 +97,13 @@ app.get('/', function(req, res) {
 
 app.get('/bundle.js', browserify(__dirname + '/client.js'));
 
-app.get('/channel_depth', function(req, res) {
+app.get('/channel_depth', channelHandler('depth'));
 
-  res.writeHead(200, {
-    'Content-Type': 'multipart/x-mixed-replace;boundary="' + boundaryID + '"',
-    'Connection': 'keep-alive',
-    'Expires': 'Fri, 27 May 1977 00:00:00 GMT',
-    'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
-    'Pragma': 'no-cache'
-  });
-
-  kinect.startDepth(function(buf) {
-    var data = new Buffer(width * height * 4);
-
-    for(var i=0; i < width * height; i++){
-
-        var depth = (buf[2*i]+buf[2*i+1]*255)/5;
-        data[4*i] = depth;
-        data[4*i+1] = depth;
-        data[4*i+2] = depth;
-        data[4*i+3] = 255;
-    }
-
-    res.write('--' + boundaryID + '\r\n')
-    res.write('Content-Type: image/jpeg\r\n');
-    res.write('Content-Length: ' + data.length + '\r\n');
-    res.write("\r\n");
-    res.write(Buffer(jpegJs.encode({
-        data: data,
-        width: width,
-        height: height
-    }, 80).data), 'binary');
-    res.write("\r\n");
-
-
-  });
-
-  req.on('close', function() {
-    kinect.pause();
-  });
-});
+app.get('/channel_video', channelHandler('video'));
 
 app.post('/tilt', function(req, res) {
-    kinect.titlAngle(req.params.angle||req.body.angle||0);
-    res.send({status: 'OK'})
-});
-
-app.get('/channel_video', function(req, res) {
-
-  res.writeHead(200, {
-    'Content-Type': 'multipart/x-mixed-replace;boundary="' + boundaryID + '"',
-    'Connection': 'keep-alive',
-    'Expires': 'Fri, 27 May 1977 00:00:00 GMT',
-    'Cache-Control': 'no-cache, no-store, max-age=0, must-revalidate',
-    'Pragma': 'no-cache'
-  });
-
-  kinect.startVideo(function(buf) {
-    // var array = ndarray(buf, [width, height, 3], [3, 3 * width, 1], 0);
-    var data = new Buffer(width * height * 4);
-
-    for(var i=0; i < width * height; i++){
-        data[4*i]   = buf[3*i];
-        data[4*i+1] = buf[3*i+1];
-        data[4*i+2] = buf[3*i+2];
-        data[4*i+3] = 255;
-    }
-
-    // ops.assign(
-    //   ndarray(data,
-    //     [width, height, 3],
-    //     [4, 4 * width, 1]), array);
-    // ops.assigns(
-    //   ndarray(data,
-    //     [width * height],
-    //     [4], 3), 255);
-
-    res.write('--' + boundaryID + '\r\n')
-    res.write('Content-Type: image/jpeg\r\n');
-    res.write('Content-Length: ' + data.length + '\r\n');
-    res.write("\r\n");
-    res.write(Buffer(jpegJs.encode({
-        data: data,
-        width: width,
-        height: height
-    }, 80).data), 'binary');
-    res.write("\r\n");
-
-
-  });
-
-  req.on('close', function() {
-    kinect.pause();
-  });
+  kinect.titlAngle(req.params.angle || req.body.angle || 0);
+  res.send({status: 'OK'})
 });
 
 kinect.resume();
